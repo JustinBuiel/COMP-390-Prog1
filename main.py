fc1f21e7d58ea37639da40776153354ab3835027
JustinBuiel <justinbuiel10@gmail.com>
2022-09-28 02:15:29 -0400

    add all the code in main and utility_functions

--- main.py
+++ main.py
@@ -2,7 +2,47 @@ import utility_functions
 
 
 def main():
-    print('Hi PyCharm')
+    data = open("meteorite_landings_data.txt")
+    data.readline()
+    all_data = {}
+    year_filtered = {}
+    mass_filtered = {}
+    name_label = "NAME"
+    mass_label = "MASS (g)"
+    year_label = "YEAR"
+
+    while 1:
+        line = data.readline()
+        if line == "":
+            break
+        all_data[len(all_data)] = utility_functions.get_data(line)
+
+    data.close()
+
+    for i in iter(all_data):
+        year_filtered[len(year_filtered)] = utility_functions.get_recent(all_data[i])
+        mass_filtered[len(mass_filtered)] = utility_functions.get_large(all_data[i])
+
+    year_filtered = {
+        key: value for key, value in year_filtered.items() if value is not None
+    }
+
+    mass_filtered = {
+        key: value for key, value in mass_filtered.items() if value is not None
+    }
+
+    print(f'{name_label:<24}{mass_label:<20}')
+    print("========================================")
+
+    for i in mass_filtered:
+        print(f'{mass_filtered[i]["name"]:<24}{mass_filtered[i]["mass"]:<20}')
+
+    print("\n\n")
+
+    print(f'{name_label:<26}{year_label:<14}')
+    print("====================================")
+    for i in year_filtered:
+        print(f'{year_filtered[i]["name"]:<26}{year_filtered[i]["year"]:<14}')
 
 
 if __name__ == '__main__':
--- utility_functions.py
+++ utility_functions.py
@@ -1,2 +1,64 @@
-def get_data():
-    print('Hi pycharm')
+def get_data(string):
+    temp = {
+        "name": " ",
+        "id": " ",
+        "nametype": " ",
+        "recclass": " ",
+        "mass": " ",
+        "fall": " ",
+        "year": " ",
+        "reclat": " ",
+        "reclong": " ",
+        "geolocation": " ",
+        "states": " ",
+        "counties": " "
+    }
+
+    temp["name"], string = separate_data(string)
+    temp["id"], string = separate_data(string)
+    temp["nametype"], string = separate_data(string)
+    temp["recclass"], string = separate_data(string)
+    temp["mass"], string = separate_data(string)
+    temp["fall"], string = separate_data(string)
+    temp["year"], string = separate_data(string)
+    temp["reclat"], string = separate_data(string)
+    temp["reclong"], string = separate_data(string)
+    temp["geolocation"], string = separate_data(string)
+    temp["states"], string = separate_data(string)
+    temp["counties"], string = separate_data(string)
+    return temp
+
+
+def get_recent(year_dict):
+    if year_dict["year"] == "N/A":
+        return
+    if 2013 <= int(year_dict["year"]) <= 2022:
+        return year_dict
+    else:
+        return
+
+
+def get_large(mass_dict):
+    if mass_dict["mass"] == "N/A":
+        return
+    if float(mass_dict["mass"]) >= 2900000.0:
+        return mass_dict
+    else:
+        return
+
+
+def separate_data(passed_string):
+    new_string = ""
+    while passed_string[0] != "\t":
+        if passed_string[0] != "\n":
+            new_string += passed_string[0]
+            passed_string = passed_string[1:]
+        else:
+            break
+
+    if new_string == "":
+        new_string = "N/A"
+    if passed_string[0] != "\n":
+        passed_string = passed_string[1:]
+    return new_string, passed_string
+
